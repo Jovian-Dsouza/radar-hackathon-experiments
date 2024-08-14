d089a24f92927bf12ad39e56468234b9d4320468
Jovian Shilton Dsouza <dsouzajovian123@gmail.com>
2024-08-14 08:17:31 +0000

    feat: add it for inititailze vault

--- tests/radar-hackathon-experiments.ts
+++ tests/radar-hackathon-experiments.ts
@@ -1,16 +1,83 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { RadarHackathonExperiments } from "../target/types/radar_hackathon_experiments";
+import {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+} from "@solana/spl-token";
+import * as fs from "fs";
+
+export async function createTokenMint(
+  provider: anchor.AnchorProvider,
+  wallet: anchor.web3.Keypair
+) {
+  const mint = await createMint(
+    provider.connection,
+    wallet, //payer
+    wallet.publicKey, //mint authoritu
+    null, //freeze authority
+    6
+  );
+  return mint;
+}
+
+export async function getRequiredATA(
+  provider: anchor.AnchorProvider,
+  wallet: anchor.web3.Keypair
+) {
+  const mintAmount = 1000 * 1e6;
+  const mint = await createTokenMint(provider, wallet);
+  console.log("Token mint: ", mint.toString());
+
+  const ata = (
+    await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet,
+      mint,
+      wallet.publicKey,
+      false
+    )
+  ).address;
+  await mintTo(
+    provider.connection,
+    wallet, //fee payer
+    mint,
+    ata,
+    wallet, //mint authority
+    mintAmount
+  );
+  return [mint, ata];
+}
+
+export async function loadWallet(
+  secretKeyPath: string
+): Promise<anchor.web3.Keypair> {
+  const secretKey = JSON.parse(fs.readFileSync(secretKeyPath, "utf8"));
+  return anchor.web3.Keypair.fromSecretKey(Uint8Array.from(secretKey));
+}
 
 describe("radar-hackathon-experiments", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.RadarHackathonExperiments as Program<RadarHackathonExperiments>;
+  const program = anchor.workspace
+    .RadarHackathonExperiments as Program<RadarHackathonExperiments>;
+  const secretKeyPath = "/home/jovian/.config/solana/id.json";
+  let wallet: anchor.web3.Keypair;
+  let provider: anchor.AnchorProvider;
+
+  before(async () => {
+    wallet = await loadWallet(secretKeyPath);
+    provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+  });
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const [mint, ata] = await getRequiredATA(provider, wallet);
+    const tx = await program.methods.initializeVault(new anchor.BN(1)).accounts({
+      mint: mint
+    }).rpc();
     console.log("Your transaction signature", tx);
   });
 });
